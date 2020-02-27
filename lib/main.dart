34c2625e02a5f71124cd15a7f722083ba5552a66
Ana Ensides <anaclara-godoy-ensides@hotmail.com>
2020-02-27 09:43:11 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart' ;
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+      child: Container(
+        color: Colors.pink,
+        child: Text("Ana Godoy",
+            style: TextStyle(color: Colors.white)
+        ),
+      ),
+    ),
+  ),
+
+);
